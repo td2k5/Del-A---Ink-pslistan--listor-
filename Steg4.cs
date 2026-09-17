beaa503dc3729be6c4ad63580a2c411e57008a4e
Tung <din-email@example.com>
2026-09-17 21:14:43 +0200

    Lägg till totalsumma

---
+++ Steg4.cs
@@ -0,0 +1,25 @@
+List<string> names = new List<string>();
+List<int> prices = new List<int>();
+
+Console.Write("Skriv en vara: ");
+string input = Console.ReadLine()!;
+
+Console.Write("Pris: ");
+string priceInput = Console.ReadLine()!;
+
+int price = int.Parse(priceInput);
+
+names.Add(input);
+prices.Add(price);
+
+Console.WriteLine("\nInköpslista:");
+
+int total = 0;
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {names[i]} - {prices[i]} kr");
+    total += prices[i];
+}
+
+Console.WriteLine($"Totalt: {total} kr");
