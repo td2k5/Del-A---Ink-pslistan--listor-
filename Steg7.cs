aa65d3e629191d9bd0b8ca9b8379ad609cd39f7d
Tung <din-email@example.com>
2026-09-18 20:15:03 +0200

    Hantera felaktiga priser

---
+++ Steg7.cs
@@ -0,0 +1,20 @@
+List<string> names = new List<string>();
+List<int> prices = new List<int>();
+
+Console.Write("Skriv en vara: ");
+string input = Console.ReadLine()!;
+
+Console.Write("Pris: ");
+string priceInput = Console.ReadLine()!;
+
+if (int.TryParse(priceInput, out int price))
+{
+    names.Add(input);
+    prices.Add(price);
+
+    Console.WriteLine("Varan har lagts till.");
+}
+else
+{
+    Console.WriteLine("Priset måste vara ett heltal. Varan lades inte till.");
+}
